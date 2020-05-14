051041c5ade2a822e03790cc1b631b35bcaf3bce
tostos <saittosun2@gmail.com>
2020-05-14 09:42:30 +0200

    template approach is finished but sondan ucuncu video da problem oldu html i yedekledim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,51 @@
+// import { Component, ViewChild } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   @ViewChild('f') signupForm: NgForm;
+//   defaultQuestion = 'pet';
+//   answer = '';
+//   genders = ['male', 'female'];
+
+//   suggestUserName() {
+//     const suggestedName = 'Superuser';
+//     // this is not good approach
+//     // this.signupForm.setValue({
+//     //   userData: {
+//     //     username: suggestedName,
+//     //     email: ''
+//     //   },
+//     //   secret: 'pet',
+//     //   questionAnswer: '',
+//     //   gender: 'male'
+//     // });
+
+//     // this is better approach
+//     // this.signupForm.form.patchValue({
+//     //   userData: {
+//     //     userName: suggestedName
+//     //   }
+//     // });
+//     console.log('clicked')
+//   }
+
+//   // onSubmit(form: NgForm) {
+//   //   console.log(form);
+//   // }
+
+//   onSubmit() {
+//     console.log(this.signupForm);
+//   }
+// }
+
+
+///////////////////////////////////
+
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -7,13 +55,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', {static: false}) signupForm: NgForm;
-  defaultQuestion = 'pet';
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -21,6 +91,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
